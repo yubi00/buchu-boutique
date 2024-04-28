47223be44b5266e62794aba5cb8734143fa73f27
yuba.khadka <yuba.khadka@fifthdomain.com.au>
2024-04-28 17:46:12 +1000

    setup stripe order payment

---
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -0,0 +1,130 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { formatCurrency } from '@/lib/formatters';
+import {
+  Elements,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import { error } from 'console';
+import Image from 'next/image';
+import { useState } from 'react';
+
+type CheckoutFormProps = {
+  product: {
+    name: string;
+    description: string;
+    priceInCents: number;
+    imagePath: string;
+  };
+  clientSecret: string;
+};
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string
+);
+
+export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
+  return (
+    <div className="max-w-5xl w-full mx-auto space-y-8">
+      <div className="flex gap-4 items-center">
+        <div className="aspect-video flex-shrink-0 w-1/3 relative">
+          <Image
+            src={product.imagePath}
+            alt={product.name}
+            fill
+            className="object-cover"
+          />
+        </div>
+        <div>
+          <div className="text-lg">
+            {formatCurrency(product.priceInCents / 100)}
+          </div>
+          <h1 className="text-2xl font-bold">{product.name} </h1>
+          <div className="line-clamp-3 text-muted-foreground">
+            {product.description}
+          </div>
+        </div>
+      </div>
+
+      <Elements options={{ clientSecret }} stripe={stripePromise}>
+        <Form priceInCents={product.priceInCents} />
+      </Elements>
+    </div>
+  );
+}
+
+function Form({ priceInCents }: { priceInCents: number }) {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!stripe || !elements) return;
+
+    setIsLoading(true);
+
+    // check for existing order
+
+    stripe
+      .confirmPayment({
+        elements,
+        confirmParams: {
+          return_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/stripe/purchase-success`,
+        },
+      })
+      .then(({ error }) => {
+        if (error.type === 'card_error' || error.type === 'validation_error') {
+          setErrorMessage(error.message!);
+        } else {
+          setErrorMessage('An unexpected error occured.');
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Checkout</CardTitle>
+          {errorMessage && (
+            <CardDescription className="text-destructive">
+              {errorMessage}
+            </CardDescription>
+          )}
+        </CardHeader>
+      </Card>
+      <CardContent>
+        <PaymentElement />
+      </CardContent>
+      <CardFooter>
+        <Button
+          className="w-full"
+          size="lg"
+          disabled={stripe == null || elements == null || isLoading}
+        >
+          {isLoading
+            ? 'Purchasing...'
+            : `Purcahse - ${formatCurrency(priceInCents / 100)}`}
+        </Button>
+      </CardFooter>
+    </form>
+  );
+}
--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,7 +1,37 @@
-export default function Purchasepage() {
+import db from '@/db/db';
+import { notFound } from 'next/navigation';
+import Stripe from 'stripe';
+import { CheckoutForm } from './_components/CheckoutForm';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+export default async function Purchasepage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const product = await db.product.findUnique({ where: { id } });
+  console.log(product);
+  if (product === null) return notFound();
+
+  // create stripe payment intent
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: 'AUD',
+    metadata: {
+      productId: product.id,
+    },
+  });
+
+  console.log(paymentIntent);
+
+  if (paymentIntent.client_secret === null)
+    throw new Error('Stripe failed to create a payment intent.');
+
   return (
-    <div>
-      <h1>Purchase Page</h1>
-    </div>
+    <CheckoutForm
+      product={product}
+      clientSecret={paymentIntent.client_secret}
+    />
   );
 }
